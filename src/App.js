db5a53d733d55da439e22ae8c65f282c96609ebc
Oz <osvaldogioia@hotmail.com>
2020-06-10 18:06:50 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Terminal from 'react-console-emulator'
+import qs from 'qs'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const commands = {
+  echo: {
+    description: 'Echo a passed string.',
+    usage: 'echo <string>',
+    fn: function () {
+      return `${Array.from(arguments).join(' ')}`
+    }
+  },
+  status: {
+    description: 'Status of Server.',
+    usage: 'status',
+    fn: function () {
+      axios({
+        method: 'post',
+        url: 'https://cors-anywhere.herokuapp.com/https://rcon-express.herokuapp.com/',
+        data: qs.stringify({
+          host: '18.230.97.82',
+          port: '27015',
+          pass: 'SomosChoborra',
+          command: 'status'
+        }),
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+      })
+      .then((result) => {
+        return result
+      })
+      .catch((err) => {
+        return `error`
+      })
+
+
+
+    }
+  }
 }
 
-export default App;
+export default class MyTerminal extends Component {
+  render () {
+    return (
+      <Terminal
+        commands={commands}
+        welcomeMessage={'Welcome to the React terminal!'}
+        promptLabel={'me@React:~$'}
+      />
+    )
+  }
+}
